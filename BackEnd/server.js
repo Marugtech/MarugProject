c92b6775dee7e0e0ae2b62ae4fdc0bfe392c374c
satish ammasi <satish.ammasi@neebal.com>
2022-08-17 18:04:39 +0530

    Latest Changes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,1 +1,13 @@
-console.log("Hi im a node developer");
+const express = require('express');
+
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("im working")
+})
+
+
+app.listen(8000, ()=>{
+    console.log('server is running 8000');
+})
