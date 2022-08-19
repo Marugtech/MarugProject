84880ba5dfe81274cc7d1aecda561c002d1e839d
satish ammasi <satish.ammasi@neebal.com>
2022-08-20 02:58:59 +0530

    Env Changes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const userRouter = require('./routers/userRoute');
+const cors = require('cors');
 require('dotenv').config();
 const mongoose = require('mongoose');
 
