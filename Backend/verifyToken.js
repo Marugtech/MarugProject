b8b617b5eeb3ba86a58852088621e40560f200ad
satish ammasi <satish.ammasi@neebal.com>
2022-08-25 19:39:12 +0530

    latest UIII

--- Backend/verifyToken.js
+++ Backend/verifyToken.js
@@ -3,7 +3,7 @@ const { errorHandler } = require('./error');
 
 const verifyToken = (req,res,next)=>{
    const token = req.cookies.auth_token;
-   if(!token) return next(errorHandler(401, "You are Not authroised"))
+   if(!token) return next(errorHandler(401, "You are Not authroised user" ))
    jwt.verify(token, process.env.TOKEN_KEY, (err,user)=>{
         if(err) return next(errorHandler(401, "Token not valid"))
         req.user = user;
